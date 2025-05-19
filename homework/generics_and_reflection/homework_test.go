f7cdc5127cd770ffb6165f6b7dce770b1250079e
gusevgrishaem1 <sibrim1212@gmail.com>
2025-05-19 23:27:33 +0400

    hw 9

--- homework/generics_and_reflection/homework_test.go
+++ homework/generics_and_reflection/homework_test.go
@@ -1,6 +1,10 @@
 package main
 
 import (
+	"bytes"
+	"reflect"
+	"strconv"
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -16,8 +20,75 @@ type Person struct {
 }
 
 func Serialize(person Person) string {
-	// need to implement
-	return ""
+	v := reflect.ValueOf(person)
+
+	var b bytes.Buffer
+
+	for i := 0; i < v.NumField(); i++ {
+		tags := getTags(v, i)
+		if len(tags) == 0 {
+			continue
+		}
+		if isOmitempty(tags, v, i) {
+			continue
+		}
+		b.WriteString(tags[0] + "=" + getValue(v.Field(i)))
+		if i < v.NumField()-1 {
+			b.WriteString("\n")
+		}
+	}
+
+	return b.String()
+}
+
+func getTags(v reflect.Value, i int) []string {
+	tags := v.Type().Field(i).Tag.Get("properties")
+	parts := strings.Split(tags, ",")
+	return parts
+}
+
+func isOmitempty(parts []string, v reflect.Value, i int) bool {
+	return len(parts) > 1 && parts[1] == "omitempty" && IsEmpty(v.Field(i))
+}
+
+func IsEmpty(rv reflect.Value) bool {
+	switch rv.Kind() {
+	case reflect.String, reflect.Array:
+		return rv.Len() == 0
+	case reflect.Map, reflect.Slice:
+		return rv.IsNil() || rv.Len() == 0
+	case reflect.Bool:
+		return !rv.Bool()
+	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
+		return rv.Int() == 0
+	case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
+		return rv.Uint() == 0
+	case reflect.Float32, reflect.Float64:
+		return rv.Float() == 0
+	case reflect.Interface, reflect.Ptr:
+		return rv.IsNil()
+	case reflect.Struct:
+		return IsEmpty(rv.Elem())
+	default:
+		return true
+	}
+}
+
+func getValue(v reflect.Value) string {
+	switch v.Kind() {
+	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
+		return strconv.Itoa(int(v.Int()))
+	case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
+		return strconv.FormatUint(v.Uint(), 10)
+	case reflect.Float32, reflect.Float64:
+		return strconv.FormatFloat(v.Float(), 'f', -1, len(v.Bytes())*8)
+	case reflect.String:
+		return v.String()
+	case reflect.Bool:
+		return strconv.FormatBool(v.Bool())
+	default:
+		return v.String()
+	}
 }
 
 func TestSerialization(t *testing.T) {
