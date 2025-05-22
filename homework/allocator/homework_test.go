5e636b45bc7c94d611a10917cc297cdf5723e599
gusevgrishaem1 <sibrim1212@gmail.com>
2025-05-22 10:54:52 +0400

    hw 10

--- homework/allocator/homework_test.go
+++ homework/allocator/homework_test.go
@@ -10,16 +10,24 @@ import (
 
 // go test -v homework_test.go
 
-func Defragment(memory []byte, pointers []unsafe.Pointer) {
+func Defragment(memory []byte, pointers []unsafe.Pointer, byteSize int) {
+	i := 0
 	j := 0
-	for i := range memory {
+	for i < len(memory) {
 		if j < len(pointers) {
-			memory[i] = *(*byte)(pointers[j])
+			pnt := pointers[j]
+			memory[i] = *(*byte)(pnt)
 			pointers[j] = unsafe.Pointer(&memory[i])
+			for k := 0; k < byteSize-1; k++ {
+				i++
+				pnt = unsafe.Add(pnt, 1)
+				memory[i] = *(*byte)(pnt)
+			}
 			j++
-			continue
+		} else {
+			memory[i] = 0x00
 		}
-		memory[i] = 0x00
+		i++
 	}
 }
 
@@ -52,7 +60,41 @@ func TestDefragmentation(t *testing.T) {
 		0x00, 0x00, 0x00, 0x00,
 	}
 
-	Defragment(fragmentedMemory, fragmentedPointers)
+	Defragment(fragmentedMemory, fragmentedPointers, 1)
+	assert.True(t, reflect.DeepEqual(defragmentedMemory, fragmentedMemory))
+	assert.True(t, reflect.DeepEqual(defragmentedPointers, fragmentedPointers))
+}
+
+func TestDefragmentationTwoByteSize(t *testing.T) {
+	var fragmentedMemory = []byte{
+		0xFF, 0xFF, 0x00, 0x00,
+		0x00, 0xFF, 0xFF, 0x00,
+		0x00, 0x00, 0xFF, 0xFF,
+		0x00, 0x00, 0xFF, 0xFF,
+	}
+
+	var fragmentedPointers = []unsafe.Pointer{
+		unsafe.Pointer(&fragmentedMemory[0]),
+		unsafe.Pointer(&fragmentedMemory[5]),
+		unsafe.Pointer(&fragmentedMemory[10]),
+		unsafe.Pointer(&fragmentedMemory[14]),
+	}
+
+	var defragmentedPointers = []unsafe.Pointer{
+		unsafe.Pointer(&fragmentedMemory[0]),
+		unsafe.Pointer(&fragmentedMemory[2]),
+		unsafe.Pointer(&fragmentedMemory[4]),
+		unsafe.Pointer(&fragmentedMemory[6]),
+	}
+
+	var defragmentedMemory = []byte{
+		0xFF, 0xFF, 0xFF, 0xFF,
+		0xFF, 0xFF, 0xFF, 0xFF,
+		0x00, 0x00, 0x00, 0x00,
+		0x00, 0x00, 0x00, 0x00,
+	}
+
+	Defragment(fragmentedMemory, fragmentedPointers, 2)
 	assert.True(t, reflect.DeepEqual(defragmentedMemory, fragmentedMemory))
 	assert.True(t, reflect.DeepEqual(defragmentedPointers, fragmentedPointers))
 }
