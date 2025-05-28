51be82978ae29ca831daa9de4b04a8ec76eae7f3
gusevgrishaem1 <sibrim1212@gmail.com>
2025-05-28 23:39:01 +0400

    hw 11

--- homework/garbage_collector/homework_test.go
+++ homework/garbage_collector/homework_test.go
@@ -10,9 +10,56 @@ import (
 
 // go test -v homework_test.go
 
+type el struct {
+	uptr uintptr
+	idx  int
+}
+
 func Trace(stacks [][]uintptr) []uintptr {
-	// need to implement
-	return nil
+	result := make([]uintptr, 0, len(stacks))
+	visited := make(map[uintptr]struct{}, len(stacks))
+	var queue []el
+
+	for i := 0; i < len(stacks); i++ {
+		for j := 0; j < len(stacks[i]); j++ {
+			if stacks[i][j] == 0 {
+				continue
+			}
+
+			if _, ok := visited[stacks[i][j]]; ok || stacks[i][j] == 0 {
+				continue
+			}
+
+			result = append(result, stacks[i][j])
+			visited[stacks[i][j]] = struct{}{}
+
+			next := (*uintptr)(unsafe.Pointer(stacks[i][j]))
+			if _, ok := visited[*next]; !ok && *next != 0 {
+				queue = append(queue, el{*next, len(result)})
+			}
+		}
+	}
+
+	for len(queue) > 0 {
+		current := queue[0]
+		queue = queue[1:]
+		if _, ok := visited[current.uptr]; ok || current.uptr == 0 {
+			continue
+		}
+
+		tmp := make([]uintptr, len(result)-current.idx)
+		copy(tmp, result[current.idx:])
+		result = result[:current.idx]
+		result = append(result, current.uptr)
+		result = append(result, tmp...)
+
+		next := (*uintptr)(unsafe.Pointer(current.uptr))
+		if _, ok := visited[*next]; !ok && *next != 0 {
+			queue = append(queue, el{*next, current.idx})
+		}
+	}
+
+	return result
 }
 
 func TestTrace(t *testing.T) {
